a52756d2809cff6e1413c7dd94452431a809cadd
unknown <aprajita602@gmail.com>
2023-08-06 17:44:53 +0530

    First Commit

---
+++ src/test/java/com/krayo/task/KrayoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krayo.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrayoTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
